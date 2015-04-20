63b13da69d466b54727279115db4ceb231c56066
Morten Holt <thawk@t-hawk.com>
2015-04-20 21:54:20 +0200

    Initial project init

---
+++ custom-page-links.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Custom Page Links
+Plugin URI: https://github.com/mkholt/custom-page-links
+Description: Set a custom list of links on a page or news post. The links can be added as a widget to a sidebar.
+Version: 1.0
+Author: morten
+Author URI: http://t-hawk.com
+License: GPL2
+*/
+
