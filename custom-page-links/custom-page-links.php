18e58b0aff4a6f05f941ab1a8036b2e81d051e04
Morten Holt <thawk@t-hawk.com>
2015-11-12 21:25:53 +0100

    Updated version and author information in plugin

--- custom-page-links/custom-page-links.php
+++ custom-page-links/custom-page-links.php
@@ -4,9 +4,9 @@
 Plugin Name: Custom Page Links
 Plugin URI: https://github.com/mkholt/custom-page-links
 Description: Set a custom list of links on a page or news post. The links can be added as a widget to a sidebar.
-Version: 1.0
-Author: morten
-Author URI: http://t-hawk.com
+Version: 1.1
+Author: mkholt
+Author URI: https://github.com/mkholt/custom-page-links
 Textdomain: custom-page-links
 License: GPL2
 */
